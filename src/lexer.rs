1d3dd868e1a03c14c0c33f2288197ebdca973d11
Ashton Sickler <ashtonsickler@ashton-sickler.attlocal.net>
2024-07-26 20:21:55 -0500

    Added more keywords and fixed Keyword error

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,9 +1,10 @@
 use std::fmt::Debug;
 use std::iter;
 use std::iter::from_fn;
+use std::collections::HashMap;
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum TokenKind {
     Identifier,
     StringLiteral,
@@ -35,8 +36,20 @@ pub enum TokenKind {
     Star,
     Equals,
 
+    Let,
+    Func,
+    If,
+    Else,
+    Elif,
+    While,
+    Not,
+    Public,
+    Private,
+    Static,
+
     EOF
 }
+
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -46,20 +59,38 @@ pub struct Token {
 }
 
 impl Token {
-    pub fn new(value: &String, kind: TokenKind) -> Self {
-        Token {
-            value: value.to_string(),
-            kind: kind
-        }
+    pub fn new(value: &String, kind: TokenKind) -> Token {
+        Token { value: value.to_string(), kind, }
     }
 }
 
-pub fn tokenize(input: String) -> Vec<Token> {
+fn init_keywords() -> HashMap<String, TokenKind> {
+    let mut keywords: HashMap<String, TokenKind> = HashMap::new();
+
+    keywords.insert("let".to_string(), TokenKind::Let);
+    keywords.insert("func".to_string(), TokenKind::Func);
+    keywords.insert("if".to_string(), TokenKind::If);
+    keywords.insert("else".to_string(), TokenKind::Else);
+    keywords.insert("elif".to_string(), TokenKind::Elif);
+    keywords.insert("while".to_string(), TokenKind::While);
+    keywords.insert("not".to_string(), TokenKind::Not);
+    keywords.insert("public".to_string(), TokenKind::Public);
+    keywords.insert("private".to_string(), TokenKind::Private);
+    keywords.insert("static".to_string(), TokenKind::Static);
+
+    return keywords;
+}
+
+pub fn tokenize<'a>(input: String) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut iter = input.chars().peekable();
 
+    let keywords = init_keywords();
+
+
     while let Some(ch) = iter.next() {
         match ch {
+            ch if ch.is_whitespace() => continue,
             '(' => tokens.push(Token::new(&ch.to_string(), TokenKind::OpenParen)),
             ')' => tokens.push(Token::new(&ch.to_string(), TokenKind::CloseParen)),
             '[' => tokens.push(Token::new(&ch.to_string(), TokenKind::OpenBrack)),
@@ -97,15 +128,19 @@ pub fn tokenize(input: String) -> Vec<Token> {
                     .collect::<String>()
                     .parse()
                     .unwrap();
-
-                tokens.push(Token::new(&c, TokenKind::Identifier))
+                if keywords.contains_key(&c) {
+                    tokens.push(Token::new(&c, keywords.get(&c).unwrap().clone()));
+                }
+                else {
+                    tokens.push(Token::new(&c, TokenKind::Identifier))
+                }
             }
             '\'' | '"' => {
                 let mut c: String = iter::once(ch)
-                    .chain(from_fn(|| iter.by_ref().next_if(|s|(    
-                        s.is_ascii_alphanumeric() || 
-                        s == &'_' ||
-                        s.is_whitespace()) && (s != &'\'' || 
+                    .chain(from_fn(|| iter.by_ref().next_if(|s|(
+                        s.is_ascii_alphanumeric() ||
+                            s == &'_' ||
+                            s.is_whitespace()) && (s != &'\'' ||
                         s != &'"'
                     ))))
                     .collect::<String>()
@@ -116,7 +151,6 @@ pub fn tokenize(input: String) -> Vec<Token> {
                     tokens.push(Token::new(&c, TokenKind::StringLiteral))
                 }
             }
-            ch if ch.is_whitespace() => continue,
             _ => {
                 panic!("unrecognized char");
             }
